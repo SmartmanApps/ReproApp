85f21cbb672de3aa7e41d55292dc584262f19c30
Smartman Apps <20635275+SmartmanApps@users.noreply.github.com>
2023-06-03 15:27:20 +1000

    Add project files.

---
+++ ReproApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReproApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ReproApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReproApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
